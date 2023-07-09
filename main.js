6f6778b1e6ec42ff2ce7286f29e54636e9c315eb
LFF5644 <meerschweinchen5644@gamil.com>
2023-07-09 22:18:57 +0200

    working!!!!

--- main.js
+++ main.js
@@ -7,101 +7,107 @@ const serialPort = require('serialport');
 const delimiter="\n";
 let reading="";
 
-const protocols=[
-	"UNKNOWN",
-	null,null,null,null,null,null,null,
-	"NEC",
-	null,
-	"Onkyo",
-]
-
+// lib functions
+function setTimeoutPromise(ms){return new Promise(resolve=>{
+	setTimeout(resolve,ms);
+})}
 function beep(text=""){
-	fs.writeFile("/dev/console","\007"+text,()=>{});
+	fs.writeFile("/dev/console","\x07"+text,()=>{});
 }
-function receiveData(data){
-	console.log("Microcontroller: "+data);
-	if(data.startsWith("Data: ")){
-		const rawData=data.substring("Data: ".length);
-		const protocol=Number(rawData.split("|")[0]);
-		const command=parseInt(rawData.split("|")[1],36);
+function sendByte(byte){
+	if(byte>255) byte=255;
+	else if(byte<0) byte=0;
+	else if(typeof(byte)==="boolean"&&byte) byte=255;
+	else if(typeof(byte)==="boolean"&&!byte) byte=0;
+	const buffer=Buffer.from([byte]);
+	port.write(buffer,err=>{
+		if(err) console.log(err);
+	});
+	return byte;
+}
+function getPercentToValue(percent,full=10){
+	// "full" ist der Grundwert also 100%
+	// "percent" ist wie viel Prozent von "full"
 
-		console.log(protocols[protocol],command);
-		
-		if(protocol===8&&command===7){ // volume key up
-			child_process.exec("amixer sset PCM 5%+");
-		}
-		else if(protocol===10&&command===49932){ // volume key down
-			child_process.exec("amixer sset PCM 5%-");
-		}
-		else if(protocol===10&&command===58889){ // track key up
-			let socket;
-			if(musikPlayer_use==="local") socket=socketMusikPlayer;
-			else socket=socketMusikPlayer_remote;
+	// full/100 is 1%
+	return (full/100)*percent;
+	// return percent from full
+	// example full=255, percent=50
+	// that returns 127.5
+}
+function getValueToPercent(value,full=10){
+	// "full" ist der Grundwert also 100%
+	// "value" ist der Prozentwert
+	
+	// 100%/full is 1%     
+	return (100/full)*value;
+	// return number between 0 and 100
+}
 
-			socket.emit("action-playback","nextTrack");
-		}
-		else if(protocol===8&&command===94){ // track key down
-			let socket;
-			if(musikPlayer_use==="local") socket=socketMusikPlayer;
-			else socket=socketMusikPlayer_remote;
+// normal functions
+function getTemperature(){
+	const temperature=Number(
+		child_process.execSync(`ssh ${remoteIp} cat /sys/class/thermal/thermal_zone0/temp`)
+			.toString("utf-8")
+			.trim()
+	)/1000;
 
-			socket.emit("action-playback","previousTrack");
-		}
-		else if(protocol===8&&command===71){ // change device key
-			if(musikPlayer_use==="local"){
-				musikPlayer_use="remote";
-				beep("   REMOTE   ");
-				setTimeout(beep,200,"   REMOTE   ");
-			}
-			else{
-				musikPlayer_use="local";
-				beep("   LOCAL   ");
-			}
-		}
-		else if(protocol===8&&command===69){ // pause/play
-			let socket;
-			if(musikPlayer_use==="local") socket=socketMusikPlayer;
-			else socket=socketMusikPlayer_remote;
+	// for debugging \/
+	const temperature_fake=Number(fs.readFileSync("tmp.txt","utf-8").trim());
+	return temperature;
+}
+function setFanSpeed(fanSpeedPercent){
+	let fanSpeedByte=Math.round(getPercentToValue(fanSpeedPercent,255));
+	console.log(`change fan speed to ${fanSpeedPercent}%`);
+	fanSpeedByte=sendByte(fanSpeedByte);
+	fanSpeedPercent=Math.round(getValueToPercent(fanSpeedByte,255));
+	return fanSpeedPercent;
+}
+function onTemperatureChanged(temperature){
+	let fanSpeed=dynamic.fanSpeedPercent;
+	
+	const minTemperature=38;
+	const maxTemperature=45;
+	const minFanSpeed=25;
+	const maxFanSpeed=100;
 
-			const isPlaying=musikPlayerCurrentlyPlaying[musikPlayer_use].isPlaying;
-
-			if(isPlaying) socket.emit("action-playback","pause");
-			else socket.emit("set-playback");
-		}
-	}
+	const current=minTemperature-temperature;
+	const length=minTemperature-maxTemperature;
+	fanSpeed=Math.round(getValueToPercent(current,length));
+	if(fanSpeed<minFanSpeed) fanSpeed=minFanSpeed;
+	else if(fanSpeed>maxFanSpeed) fanSpeed=maxFanSpeed;
+	console.log(`FanSpeedPercent: ${fanSpeed}%, Temperature: ${temperature}`);
+	return fanSpeed;
 }
 
 const remoteIp="192.168.178.55";
 
-let musikPlayer_use="local";
-const musikPlayerCurrentlyPlaying={
-	local:{},
-	remote:{},
-}
-const socketMusikPlayer=socketIoClient.io("http://localhost:4561");
-socketMusikPlayer.on("currentlyPlaying",currentlyPlaying=>musikPlayerCurrentlyPlaying.local=currentlyPlaying);
+const port = new serialPort.SerialPort({
+	path: "/dev/ttyACM0",
+	baudRate: 9600,
+});
 
-const socketMusikPlayer_remote=socketIoClient.io("http://"+remoteIp+":4561");
-socketMusikPlayer_remote.on("currentlyPlaying",currentlyPlaying=>musikPlayerCurrentlyPlaying.remote=currentlyPlaying);
-
-const port = new serialPort.SerialPort({ path: "/dev/ttyACM0", baudRate: 9600,})
-
-port.on("open", () => {
+port.on("open",()=>{
 	console.log("serial port open");
+	setTimeout(()=> setFanSpeed(dynamic.fanSpeedPercent),1e3);
 });
 port.on("data",buffer=>{
-	const data=buffer.toString("utf-8");
-	if(data.split(delimiter).length>1){
-		receiveData(reading+data.split(delimiter)[0]);
-		if(data.split(delimiter).length>2){
-			for(const data of data.split(delimiter)){
-				receiveData(data);
-			}
-		}
-		reading="";
-	}
-	else{
-		reading+=data;
-	}
+	//process.stdout.write(buffer);
 });
 
+let dynamic={
+	fanSpeedPercent: null,
+	last_temperature: null,
+	temperature: null,
+};
+
+(async()=>{//main
+	while(true){
+		const temperature=getTemperature();
+		dynamic.temperature=temperature;
+		const fanSpeedPercent=onTemperatureChanged(temperature);
+		dynamic.last_temperature=temperature;
+		dynamic.fanSpeedPercent=setFanSpeed(fanSpeedPercent);
+		await setTimeoutPromise(500); // wait 500ms aka. 0.5s
+	}
+})();
